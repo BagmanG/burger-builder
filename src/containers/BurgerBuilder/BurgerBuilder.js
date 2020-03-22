cfaea34681caff6b3cfc9f071b8ab18fdeb052f7
Alexander B <alex.cptn3mo@gmail.com>
2020-03-22 23:29:59 +0300

    Add components with stylings

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
-    render() { 
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    };
+
+    render() {
         return (
             <Aux>
-                <div>Burger</div>
-                <div>Burger Controls</div>
+                <Burger ingredients={this.state.ingredients} />
+                <div>Build Controls</div>
             </Aux>
         );
     }
 }
- 
+
 export default BurgerBuilder;

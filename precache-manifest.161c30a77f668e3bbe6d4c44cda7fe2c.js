17051019e0b4a661a5619cbedec66b2d03873120
Alexander B <alex.cptn3mo@gmail.com>
2020-03-24 18:16:50 +0300

    Updates

---
+++ precache-manifest.161c30a77f668e3bbe6d4c44cda7fe2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e1fa7a6e38ffc2b0e750e224b20dea0",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "288d0ce56222970ffa2a",
+    "url": "/burger-builder/static/css/main.d52bb015.chunk.css"
+  },
+  {
+    "revision": "9807660797c122f76e71",
+    "url": "/burger-builder/static/js/2.6eaf75b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burger-builder/static/js/2.6eaf75b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "288d0ce56222970ffa2a",
+    "url": "/burger-builder/static/js/main.ea4e65f7.chunk.js"
+  },
+  {
+    "revision": "583042396e8cc56c1062",
+    "url": "/burger-builder/static/js/runtime-main.2a419647.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/burger-builder/precache-manifest.161c30a77f668e3bbe6d4c44cda7fe2c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/burger-builder/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});

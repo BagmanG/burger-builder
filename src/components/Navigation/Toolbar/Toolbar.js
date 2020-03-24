41a9efb1eb8b2db188b71fae6f8779d068fd5c2d
Alexander B <alex.cptn3mo@gmail.com>
2020-03-24 13:05:45 +0300

    Add mobile menu

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,13 +3,16 @@ import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = props => {
     return (
         <header className={classes.Toolbar}>
-            <div>MENU</div>
-            <Logo />
-            <nav>
+            <DrawerToggle clicked={props.drawerToggleClicked} />
+            <div className={classes.Logo}>
+                <Logo />
+            </div>
+            <nav className={classes.DesktopOnly}>
                 <NavigationItems />
             </nav>
         </header>
